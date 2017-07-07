1ee962cfb560d83a43d046221e36e072a5b8c855
Qiuyang Ye <qye@yahoo-inc.com>
2017-07-06 21:30:16 -0700

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Qiuyang Ye on 7/6/17.
+//  Copyright © 2017 Qiuyang Ye. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
